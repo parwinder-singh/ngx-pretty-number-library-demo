0520bb0d38df690cdebe563da8d816f46421f37c
Parwinder <parwinder.singh@appinventiv.com>
2019-12-17 14:31:20 +0530

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {ReactiveFormsModule} from "@angular/forms";
+import {PrettyNumberModule} from "ngx-pretty-number";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule,
+    PrettyNumberModule
   ],
   providers: [],
   bootstrap: [AppComponent]
